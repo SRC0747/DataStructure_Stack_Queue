5c515d6f40fa4b158f5cd033ab2ab1998a9b5ae1
SRC0747 <RjSrc@0747>
2021-08-20 22:33:22 +0530

    Welcome to Stack and Queue Problem.

---
+++ src/main/java/MyStackQueue.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MyStackQueue {
+        public void given3NumbersAddedToStack(){
+            MyNode<Integer> myFirstNode = new MyNode(70);
+            MyNode<Integer> mySecondNode = new MyNode(30);
+            MyNode<Integer> myThirdNode = new MyNode(56);
+            myFirstNode.setNext(mySecondNode);
+            mySecondNode.setNext(myThirdNode);
+            boolean result = myFirstNode.getNext().equals(mySecondNode) && myFirstNode.getNext().getNext().equals(myThirdNode);
+            Assert.assertTrue(result);
+        }
+    }
+}
