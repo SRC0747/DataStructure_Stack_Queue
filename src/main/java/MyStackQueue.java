129a2bbac640080d5a3775d1a3206a53ab2a4c4e
SRC0747 <RjSrc@0747>
2021-08-20 22:50:01 +0530

    added : Pop all the elements from the Stack till it is Emopty.

--- src/main/java/MyStackQueue.java
+++ src/main/java/MyStackQueue.java
@@ -10,10 +10,26 @@ public class MyStackQueue {
             myFirstNode.setNext(mySecondNode);
             mySecondNode.setNext(myThirdNode);
             boolean result = myFirstNode.getNext().equals(mySecondNode) && myFirstNode.getNext().getNext().equals(myThirdNode);
-            myLinkedList.pop();
-            myLinkedList.pop();
-            myLinkedList.pop();
             Assert.assertTrue(result);
         }
     }
 }
+
+@Test
+/*Pop All the Elements from the Stack*/
+public class PopTheStack {
+    public void popTheStack(){
+        MyNode<Integer> myFirstNode = new MyNode(70);
+        MyNode<Integer> mySecondNode = new MyNode(30);
+        MyNode<Integer> myThirdNode = new MyNode(56);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        myLinkedList.pop();
+        myLinkedList.pop();
+        myLinkedList.pop();
+        boolean result = myFirstNode.getNext().equals(mySecondNode) && myFirstNode.getNext().getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+}
+}
+
